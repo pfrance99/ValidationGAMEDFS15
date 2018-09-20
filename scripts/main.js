9e38674cfe8517f2497e21c50aff46a5fefdb63c
admin <admin@iMac3-5.local>
2018-09-20 15:15:53 +0200

    DEV: Ajout d'une perte de point auto si pas de clic

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,15 @@
 $(document).ready(function() {
 
+  const RELOAD_TIME = 700; //vitesse d'apparition des points
   var colors = ['#A00000', '#650068', '#230A6E', '#008000', '#809A00', '#A08400', '#A05A00'];
-  var firstColor, secondColor, timer, similarity, score;
+  var firstColor, secondColor, timer, similarity;
   var clicked = false;
+  var score = 0;
 
 
+  $('#score-actual').html(score); //Initialisation du score dans le html
+
+  //Fonction appellée lors du clic sur le bouton de lancement
   $('#start-game').on('click', function () {
     $('.square').removeClass('presentation');
     $('#start-game').attr('disabled', 'disabled');
@@ -17,30 +22,32 @@ $(document).ready(function() {
     similarity = firstColor === secondColor;
     $('#square1').css('background-color', firstColor);
     $('#square2').css('background-color', secondColor);
+    //Interval gérant le rechargement des points toutes les RELOAD_TIME secondes
     timer = setInterval(function () {
+      if(similarity && !clicked){
+        score--;
+        $('#score-actual').html(score);
+      }
       clicked = false;
       firstColor = colors[getRandomInt(colors.length)];
       secondColor = colors[getRandomInt(colors.length)];
       similarity = firstColor === secondColor;
       $('#square1').css('background-color', firstColor);
       $('#square2').css('background-color', secondColor);
-    }, 1000);
+    }, RELOAD_TIME);
   });
 
+  //Fonction appellée lors du clic sur le bouton de réinitialisation
   $('#stop-game').on('click', function () {
-    $('.square').addClass('presentation');
-    $('#start-game').removeAttr("disabled");
-    clearInterval(timer);
-    $('#square1').css('background-color', '#447684');
-    $('#square2').css('background-color', '#447684');
-    $('.buzzer').css('display', 'none');
+    reinitGame();
   });
 
+  //Fonction appellée lors du click sur le buzzer
   $('.buzzer').on('click', function () {
-    if(clicked) {
+    if(clicked) { //Dans le cas ou tu as un doublon mais tu as déja pressé le buzzer sur ce doublon
       return;
     }
-    if(similarity) {
+    if(similarity) { //Si tu appuyes et qu'il y a un doublon
       score++;
       $('#score-actual').html(score);
       clicked = true;
@@ -48,7 +55,7 @@ $(document).ready(function() {
       setTimeout(function () {
         $('.square').css('border', '10px solid white');
       }, 100);
-    } else {
+    } else { //Si tu appuyes alors qu'il n'y a pas de doublons de couleurs
       score--;
       $('#score-actual').html(score);
       $('.square').css('border', '10px solid red');
@@ -58,8 +65,21 @@ $(document).ready(function() {
     }
   });
 
-  function getRandomInt(max) {
+  /** Retourne un entier aléatoire compris entre 0 et max
+   * @param {Number} max entier majorant l'interval de random sans être compris
+   * @return {Number} entier aléatoire entre 0 et max-1
+   */
+  var getRandomInt = function (max) {
     return Math.floor(Math.random() * Math.floor(max));
   }
 
+  //Réinitialise le jeu à l'état initial
+  var reinitGame = function () {
+    $('.square').addClass('presentation');
+    $('#start-game').removeAttr("disabled");
+    clearInterval(timer);
+    $('#square1').css('background-color', '#447684');
+    $('#square2').css('background-color', '#447684');
+    $('.buzzer').css('display', 'none');
+  }
 });
